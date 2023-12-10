b3cf0ccc30506cfdca7f1bf15322cbf6118f8bd8
arifine <sultanularifine@gmail.com>
2023-12-10 21:50:53 +0600

    Login & Register Page are Created

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -1,12 +1,56 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Blog;
+use App\Models\User;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
 class BlogController extends Controller
 {
     public function index(){
         return view ('blog.index');
+    }
+    public function loginForm()
+    {
+
+        return view('blog.login');
+    }
+
+    public function register()
+    {
+
+        return view('blog.register');
+    }
+    public function registerStore(Request $request)
+    {
+
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        $blog = new User();
+        $blog->name = $request->name;
+        $blog->email = $request->email;
+        $blog->password = $request->password;
+
+        if ($blog->save()) {
+            return redirect()->route('blog.loginForm');
+        }
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            
+            return redirect()->route('blog.index');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ]);
     }
 }
--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -30,10 +30,10 @@
                     <a class="nav-link" href="/profile">Profile</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/login">Login</a>
+                    <a class="nav-link" href="{{ route('blog.loginForm') }}">Login</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/register">Register</a>
+                    <a class="nav-link" href="{{ route('blog.register') }}">Register</a>
                 </li>
             </ul>
         </div>
